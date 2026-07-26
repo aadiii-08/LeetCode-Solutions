724e8a013b686a53894b90d93a894ec133f2d82a
Aditya Raj <adityaraj825271@gmail.com>
2026-07-26 18:12:07 +0530

    Time: 2 ms (99.61%) | Memory: 47.2 MB (89.75%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,12 +1,33 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        int n = nums.length - 1;
-        int ans = 0;
-        Arrays.sort(nums);
+        int n = nums.length;
+        int pos1 = Integer.MIN_VALUE, pos2 = Integer.MIN_VALUE, pos3 = Integer.MIN_VALUE;
+        int neg1 = Integer.MAX_VALUE, neg2 = Integer.MAX_VALUE;
 
-        int val1 = nums[0] * nums[1] * nums[n];
-        int val2 = nums[n] * nums[n - 1] * nums[n - 2];
-        ans = Math.max(val1 , val2);
-        return ans;
+        for(int i = 0; i < n; i++){
+            int curr = nums[i];
+
+            if(curr >= pos1){
+                pos3 = pos2;
+                pos2 = pos1;
+                pos1 = curr;
+            }else if(curr >= pos2){
+                pos3 = pos2;
+                pos2 = curr;
+            }else if(curr > pos3){
+                pos3 = curr;
+            }
+
+            if(curr <= neg1){
+                neg2 = neg1;
+                neg1 = curr;
+            }else if(curr < neg2){
+                neg2 = curr;
+            }
+        }
+
+        int val1 = pos1 * pos2 * pos3;
+        int val2 = neg1 * neg2 * pos1;
+        return Math.max(val1 , val2);
     }
 }
