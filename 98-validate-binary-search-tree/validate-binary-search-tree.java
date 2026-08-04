6d0dca7dd30acb18217b4fbc1bfee51c6a5dbca0
Aditya Raj <adityaraj825271@gmail.com>
2026-08-05 00:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (34.97%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,37 +1,15 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    TreeNode prev = null;
+
     public boolean isValidBST(TreeNode root) {
         if(root == null) return true;
-        
-        List<Integer> list = new ArrayList<>();
-        inorder(root, list);
+        if(!isValidBST(root.left)) return false;
 
-        for(int i = 1; i < list.size(); i++){
-            if(list.get(i - 1) >= list.get(i)) return false;
-        }
+        if(prev != null && prev.val >= root.val) return false;
+        prev = root;
 
+        if(!isValidBST(root.right)) return false;
         return true;
-    }
 
-    void inorder(TreeNode root, List<Integer> list){
-        if(root == null) return;
-
-        inorder(root.left, list);
-        list.add(root.val);
-        inorder(root.right, list);
     }
 }
