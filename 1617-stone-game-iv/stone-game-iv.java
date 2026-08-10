410f750743f47bc08ae6c2267a99248daee3835b
Aditya Raj <adityaraj825271@gmail.com>
2026-08-10 22:04:47 +0530

    Time: 14 ms (91.38%) | Memory: 42.3 MB (83.55%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,25 +1,16 @@
 class Solution {
-    List<Integer> t = new ArrayList<>();
     public boolean winnerSquareGame(int n) {
-       for(int i = 0; i <= n; i++) {
-            t.add(-1);
-        }
-        return solve(n);
-    }
-
-    boolean solve(int n){
-        if(n == 0) return false;
-
-        if(t.get(n) != -1){
-            return t.get(n) == 1;
-        }
-        for(int k = 1; k * k <= n; k++){
-            if(solve(n - (k*k)) == false){
-                t.set(n, 1);
-                return true;
+        boolean[] t = new boolean[n+1];
+        
+        for(int i = 1; i < n + 1; i++){
+            for(int k = 1; k * k <= i; k++){
+                if(t[i - k * k] == false){
+                    t[i] = true;
+                    break;
+                }
             }
         }
-        t.set(n, 0);
-        return  false;
+
+        return t[n];
     }
 }
