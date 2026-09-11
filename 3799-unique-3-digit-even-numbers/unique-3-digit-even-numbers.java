552aabbacffae28657715be9cf62dc297b897aa3
Aditya Raj <adityaraj825271@gmail.com>
2026-09-11 19:08:26 +0530

    Time: 6 ms (50.40%) | Memory: 44.2 MB (88.27%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,22 +1,36 @@
 class Solution {
     public int totalNumbers(int[] digits) {
         int n = digits.length;
-        HashSet<Integer> set = new HashSet<>();
+        int[] freq = new int[10];
+        for(int digit : digits){
+            freq[digit]++;
+        }
 
-        for(int i = 0; i < n; i++){
-            if(digits[i] == 0) continue;
-            for(int j = 0; j < n; j++){
-                if(i == j) continue;
-                for(int k = 0; k < n; k++){
-                    if(i == k) continue;
-                    if(j == k) continue;
-                    if(digits[k] % 2 == 0){
-                        set.add((digits[i] * 100) + (digits[j] * 10) + digits[k]);
+        int count = 0;
+
+        for(int i = 1; i <= 9; i++){
+            for(int j = 0; j < 10; j++){
+                for(int k = 0; k <= 8; k += 2){
+                    // Temporarily use the digits
+                    freq[i]--;
+                    freq[j]--;
+                    freq[k]--;
+
+                    // If all frequencies are >= 0,
+                    // the number can be formed
+                    if (freq[i] >= 0 &&
+                        freq[j] >= 0 &&
+                        freq[k] >= 0) {
+                        count++;
                     }
+
+                    // Restore frequencies
+                    freq[i]++;
+                    freq[j]++;
+                    freq[k]++;
                 }
             }
         }
-
-        return set.size();
+        return count;
     }
 }
