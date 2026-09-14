017f741ffc069c553cd27603a7def8d860719af7
Aditya Raj <adityaraj825271@gmail.com>
2026-09-14 17:21:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (52.92%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.java
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -3,8 +3,13 @@ class Solution {
         int x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3];
         int x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
 
-        if(x3 >= x2 || y3 >= y2 || x1 >= x4 || y1 >= y4) return false;
+        // method 1
+        // if(x3 >= x2 || y3 >= y2 || x1 >= x4 || y1 >= y4) return false;
 
-        return true;
+        // return true;
+
+        // method 2
+
+        return Math.min(x2, x4) > Math.max(x1, x3) && Math.min(y2, y4) > Math.max(y1, y3);
     }
 }
