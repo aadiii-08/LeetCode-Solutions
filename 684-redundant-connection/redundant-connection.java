92a12f79a7428b0a8670b01079886620cc8c982e
Aditya Raj <adityaraj825271@gmail.com>
2026-09-17 01:14:04 +0530

    Time: 1 ms (91.36%) | Memory: 45 MB (68.48%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,52 @@
+class Solution {
+    int totalNodes;
+    public int[] findRedundantConnection(int[][] edges) {
+        totalNodes = edges.length;
+        int[] res = new int[2];
+        DisjointSet dsu = new DisjointSet(totalNodes);
+        for(int[] edge : edges){
+            if(!dsu.unionBySize(edge[0] - 1, edge[1] - 1)){
+                res = edge;
+            }
+        }
+
+        return res;
+    }
+
+    public class DisjointSet{
+        int[] parent;
+        int[] size;
+        DisjointSet(int node){
+            this.parent = new int[node];
+            this.size = new int[node];
+            for(int i = 0; i < node; i++){
+                this.parent[i] = i;
+                this.size[i] = 1;
+            }
+        }
+
+        public int findRootParent(int node){
+            if(node == parent[node]){
+                return node;
+            }
+            return parent[node] = findRootParent(parent[node]);
+        }
+
+        public boolean unionBySize(int node1, int node2){
+            int rootParent1 = findRootParent(node1);
+            int rootParent2 = findRootParent(node2);
+            if(rootParent1 == rootParent2){
+                return false;
+            }
+
+            if(size[rootParent1] < size[rootParent2]){
+                parent[rootParent1] = rootParent2;
+                size[rootParent2] += size[rootParent1];
+            }else{
+                parent[rootParent2] = rootParent1;
+                size[rootParent1] += size[rootParent2];
+            }
+            return true;
+        }
+    }
+}
