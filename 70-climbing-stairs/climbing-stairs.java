b7b3d03bea9c31d08b000de3dba8b2ab32c8a753
Aditya Raj <adityaraj825271@gmail.com>
2026-09-24 16:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.06%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -21,30 +21,46 @@
 
 
 // dp + memoization
+// class Solution {
+//     int[] dp;
+
+//     public int climbStairs(int n) {
+//         dp = new int[46];
+//         Arrays.fill(dp, -1);
+//         return solve(n);
+//     }
+
+//     int solve(int n){
+//         if(n < 0){
+//             return 0;
+//         }
+
+//         if(n == 0){
+//             return 1;
+//         }
+
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+
+//         int oneStep = solve(n - 1);
+//         int twoStep = solve(n - 2);
+//         return dp[n] = oneStep + twoStep;
+//     }
+// }
+
+
+// bottom up
 class Solution {
-    int[] dp;
-
     public int climbStairs(int n) {
-        dp = new int[46];
-        Arrays.fill(dp, -1);
-        return solve(n);
-    }
+        if(n == 1 || n == 2) return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
 
-    int solve(int n){
-        if(n < 0){
-            return 0;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-
-        if(n == 0){
-            return 1;
-        }
-
-        if(dp[n] != -1){
-            return dp[n];
-        }
-
-        int oneStep = solve(n - 1);
-        int twoStep = solve(n - 2);
-        return dp[n] = oneStep + twoStep;
+        return dp[n];
     }
 }
