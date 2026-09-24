bfcdc8eaa4f19c5d9cc2957ace42815878645c32
Aditya Raj <adityaraj825271@gmail.com>
2026-09-24 16:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.68%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -51,16 +51,35 @@
 
 
 // bottom up
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n == 1 || n == 2) return n;
+//         int[] dp = new int[n + 1];
+//         dp[1] = 1;
+//         dp[2] = 2;
+
+//         for(int i = 3; i <= n; i++){
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+
+// simple using two variable as previous track in costant space
 class Solution {
     public int climbStairs(int n) {
         if(n == 1 || n == 2) return n;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
 
+        int a = 1; 
+        int b = 2;
+        int c = 3;
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            c = b + a;
+            a = b;
+            b = c;
         }
-        return dp[n];
+
+        return c;
     }
 }
